a20297e3c42cacfedef3c192e7327356cca415cc
Michael Watson <michaelw@michaelw-mac.local>
2016-03-13 17:14:27 -0700

    Initial Commit

---
+++ Particle/ParticleCloud.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using static System.String;
+using static System.Diagnostics.Debug;
+
+using ModernHttpClient;
+using static Newtonsoft.Json.JsonConvert;
+
+using Particle.Models;
+
+namespace Particle
+{
+	public class ParticleCloud : IDisposable
+	{
+		#region Constructors
+
+		public ParticleCloud()
+		{
+			if (SharedInstance != null)
+				throw new Exception("You can only create one instance of the ParticleCloud");
+
+			OAuthClientId = "particle";
+			OAuthClientSecret = "particle";
+			SharedInstance = this;
+		}
+
+		public ParticleCloud(string accessToken, string refreshToken, DateTime expiration)
+		{
+			if (SharedInstance != null)
+				throw new Exception("You can only create one instance of the ParticleCloud");
+			
+			AccessToken = new ParticleAccessToken(accessToken, refreshToken, expiration);
+			SharedInstance = this;
+			OAuthClientId = "particle";
+			OAuthClientSecret = "particle";
+		}
+
+		#endregion
+
+		#region Public Properties
+
+		public static ParticleCloud SharedInstance;
+		public string LoggedInUsername { get; internal set; }
+		public bool IsLoggedIn { get; internal set; }
+		public static ParticleAccessToken AccessToken { get; set; }
+		public string OAuthClientId { get; internal set; }
+		public string OAuthClientSecret { get; internal set; }
+
+		#endregion
+
+		#region IDisposable Implementation
+
+		public void Dispose()
+		{
+			LoggedInUsername = null;
+			AccessToken = null;
+			OAuthClientId = null;
+			OAuthClientSecret = null;
+			SharedInstance = null;
+		}
+
+		#endregion
+
+		#region Public Methods
+		/// <summary>
+		/// Creates the OAuth client and sets the OAuth Client Id and Secret if successful.
+		/// </summary>
+		/// <returns>A boolean indicating whether the OAuth client was created successfully or not.</returns>
+		/// <param name="accessToken">Access token.</param>
+		/// <param name="appName">Authorized app name.</param>
+		public async Task<bool> CreateOAuthClientAsync(string accessToken, string appName)
+		{
+			var requestContent = new FormUrlEncodedContent(new[] {
+				new KeyValuePair<string, string> ("name", appName),
+				new KeyValuePair<string, string> ("type", "installed"),
+				new KeyValuePair<string, string> ("access_token", accessToken),
+			});
+
+			try
+			{
+				using (var client = new HttpClient(new NativeMessageHandler()))
+				{
+					var response = await client.PostAsync("https://api.particle.io/v1/clients", requestContent);
+					var particleResponse = DeserializeObject<ParticleOAuthResponse>(await response.Content.ReadAsStringAsync());
+
+					if (particleResponse.Success)
+					{
+						OAuthClientId = particleResponse.ClientDetails.Id;
+						OAuthClientSecret = particleResponse.ClientDetails.Secret;
+						return true;
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				WriteLine(e.Message);
+			}
+			return false;
+		}
+		/// <summary>
+		/// Log in with a given username or password.
+		/// </summary>
+		/// <returns>A boolean indicating whether the login attempt was successful or not.</returns>
+		/// <param name="username">Username.</param>
+		/// <param name="password">Password.</param>
+		public async Task<bool> LoginWithUserAsync(string username, string password)
+		{
+			var requestContent = new FormUrlEncodedContent(new[] {
+				new KeyValuePair<string, string> ("grant_type", "password"),
+				new KeyValuePair<string, string> ("username", username),
+				new KeyValuePair<string, string> ("password", password),
+				new KeyValuePair<string, string> ("client_id", OAuthClientId),
+				new KeyValuePair<string, string> ("client_secret", OAuthClientSecret),
+			});
+
+			try
+			{
+				using (var client = new HttpClient(new NativeMessageHandler()))
+				{
+					var response = await client.PostAsync("https://api.particle.io/oauth/token", requestContent);
+					var particleResponse = DeserializeObject<ParticleLoginResponse>(await response.Content.ReadAsStringAsync());
+
+					if (particleResponse.AccessToken != null || !IsNullOrEmpty(particleResponse.AccessToken))
+					{
+						AccessToken = new ParticleAccessToken(particleResponse);
+						IsLoggedIn = true;
+						return true;
+					}
+				}
+
+			}
+			catch (Exception e)
+			{
+				WriteLine(e.Message);
+			}
+			return false;
+		}
+		/// <summary>
+		/// Logout the current user.
+		/// </summary>
+		public void Logout()
+		{
+			AccessToken = null;
+			LoggedInUsername = null;
+			IsLoggedIn = false;
+		}
+		/// <summary>
+		/// Sign-up a new user with Particle.
+		/// </summary>
+		/// <returns>A string of either "Success" or the specific error</returns>
+		/// <param name="username">Username.</param>
+		/// <param name="password">Password.</param>
+		public async Task<string> SignupWithUserAsync(string username, string password)
+		{
+			ParticleGeneralResponse particleResponse = null;
+
+			var requestContent = new FormUrlEncodedContent(new[] {
+				new KeyValuePair<string, string> ("username", username),
+				new KeyValuePair<string, string> ("password", password),
+			});
+
+			try
+			{
+				using (var client = new HttpClient(new NativeMessageHandler()))
+				{
+					var response = await client.PostAsync("https://api.particle.io/v1/users", requestContent);
+					particleResponse = DeserializeObject<ParticleGeneralResponse>(await response.Content.ReadAsStringAsync());
+
+					if (particleResponse.Ok)
+						return "Success";
+				}
+			}
+			catch (Exception e)
+			{
+				WriteLine(e.Message);
+			}
+
+			return particleResponse.Errors[0] ?? "UnknownError";
+		}
+
+		//TODO Still need to complete
+		public void SignUpWithCusomter(string email, string password, string orgSlig)
+		{
+		}
+		public void RequestPasswordResetForCustomer(string orgSlug, string email)
+		{
+		}
+		public void RequestPasswordResetForUser(string email)
+		{
+		}
+		/// <summary>
+		/// Gets a list of the users registered Particle Devices..
+		/// </summary>
+		/// <returns>A list of the users Particle Devices.</returns>
+		public async Task<List<ParticleDevice>> GetDevicesAsync()
+		{
+			if (IsNullOrEmpty(AccessToken.Token) || AccessToken.Token == "expired")
+				return null;
+
+			try
+			{
+				using (var client = new HttpClient(new NativeMessageHandler()))
+				{
+					var response = await client.GetAsync("https://api.spark.io/v1/devices/?access_token=" + AccessToken.Token);
+					var responseText = await response.Content.ReadAsStringAsync();
+
+					if (responseText.Contains("error"))
+						return null;
+
+					var particleArgs = DeserializeObject<List<ParticleDeviceObject>>(responseText);
+					var particleDevices = new List<ParticleDevice>();
+					foreach (var device in particleArgs)
+						particleDevices.Add(new ParticleDevice(device));
+
+					return particleDevices;
+				}
+
+			}
+			catch (Exception e)
+			{
+				WriteLine(e.Message);
+			}
+
+			return null;
+		}
+		/// <summary>
+		/// Gets a Particle Devices from it's unique ID.
+		/// </summary>
+		/// <returns>The Particle Device.</returns>
+		/// <param name="deviceId">Particle Device Unique Id.</param>
+		public async Task<ParticleDevice> GetDeviceAsync(string deviceId)
+		{
+			try
+			{
+				using (var client = new HttpClient(new NativeMessageHandler()))
+				{
+					var response = await client.GetAsync(
+						"https://api.spark.io/v1/devices/" + deviceId + "/?access_token=" + AccessToken.Token);
+					var particleArgs = DeserializeObject<ParticleDeviceObject>(await response.Content.ReadAsStringAsync());
+
+					if (particleArgs != null)
+						return new ParticleDevice(particleArgs);
+				}
+
+			}
+			catch (Exception e)
+			{
+				WriteLine(e.Message);
+				return null;
+			}
+
+			return null;
+		}
+		/// <summary>
+		/// Refresh the ParticleCloud Access Token. The AccessToken on the ParticleCloud instance will be updated. 
+		/// A return value of Null indicates the refresh was unsuccessful.
+		/// </summary>
+		/// <returns>The AccessToken.</returns>
+		/// <param name="appName">App name.</param>
+		public async Task<ParticleAccessToken> RefreshTokenAsync(string appName)
+		{
+			var requestContent = new FormUrlEncodedContent(new[] {
+				new KeyValuePair<string, string> ("name", appName),
+				new KeyValuePair<string, string> ("type", "installed"),
+				new KeyValuePair<string, string> ("grant_type", "refresh_token"),
+				new KeyValuePair<string, string> ("client_id", OAuthClientId),
+				new KeyValuePair<string, string> ("client_secret", OAuthClientSecret),
+				new KeyValuePair<string, string> ("access_token", AccessToken.Token),
+				new KeyValuePair<string, string> ("refresh_token", AccessToken.RefreshToken),
+			});
+
+			try
+			{
+				using (var client = new HttpClient(new NativeMessageHandler()))
+				{
+					var response = await client.PostAsync(
+						"https://api.particle.io/oauth/token/?refresh_token=" + AccessToken.RefreshToken,
+						requestContent
+					);
+
+					var particleResponse = DeserializeObject<ParticleLoginResponse>(await response.Content.ReadAsStringAsync());
+
+					if (particleResponse.AccessToken != null || !IsNullOrEmpty(particleResponse.AccessToken))
+					{
+						AccessToken = new ParticleAccessToken(particleResponse);
+						IsLoggedIn = true;
+						return AccessToken;
+					}
+				}
+
+			}
+			catch (Exception e)
+			{
+				WriteLine(e.Message);
+			}
+
+			return null;
+		}
+		/// <summary>
+		/// Claims an unregistered Particle Device.
+		/// </summary>
+		/// <returns>A boolean indicating whether the device was successfully claimed or not.</returns>
+		/// <param name="deviceId">Particle Device Unique Id.</param>
+		public async Task<bool> ClaimDeviceAsync(string deviceId)
+		{
+			var requestContent = new FormUrlEncodedContent(new[] { new KeyValuePair<string, string>("id", deviceId) });
+
+			try
+			{
+				using (var client = new HttpClient(new NativeMessageHandler()))
+				{
+					var response = await client.PostAsync(
+						"https://api.spark.io/v1/devices?access_token=" + AccessToken.Token,
+						requestContent);
+
+					var particleResponse = DeserializeObject<ParticleFunctionResponse>(await response.Content.ReadAsStringAsync());
+
+					if (particleResponse.Connected)
+						return true;
+				}
+			}
+			catch (Exception e)
+			{
+				WriteLine(e.Message);
+			}
+
+			return false;
+		}
+
+		#endregion
+	}
+}
---
+++ Particle/ParticleDevice.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using static System.String;
+using static System.Diagnostics.Debug;
+
+using ModernHttpClient;
+using static Newtonsoft.Json.JsonConvert;
+
+using Particle.Models;
+
+namespace Particle
+{
+	public enum ParticleDeviceType
+	{
+		Core = 0,
+		Photon = 6,
+		Electron = 10
+	}
+
+	public class ParticleDevice
+	{
+		readonly int MAX_SPARK_FUNCTION_ARG_LENGTH = 63;
+
+		#region Constructors 
+
+		public ParticleDevice(ParticleDeviceObject attributes)
+		{
+			if (attributes.Name == "" || attributes.Name == null)
+				throw new ArgumentNullException("Attributes");
+
+			Id = attributes.Id;
+			Name = attributes.Name;
+			Connected = attributes.Connected;
+
+			Variables = attributes.Variables;
+			Functions = attributes.Functions;
+
+			Version = attributes.CC3000PatchVersion;
+
+			if (attributes.LastHeard != null)
+				LastHeard = DateTime.Parse(attributes.LastHeard);
+
+			RequiresUpdate = false;
+
+			if (attributes.RequiresUpdate)
+				RequiresUpdate = true;
+		}
+
+		#endregion
+
+		#region Public Properties
+
+		public string Category { get; internal set; }
+		public string Id { get; internal set; }
+		public string Name { get; internal set; }
+		public bool Connected { get; internal set; }
+		public List<string> Functions { get; internal set; }
+		public Dictionary<string, string> Variables { get; internal set; } 
+		public string LastApp { get; internal set; }
+		public DateTime LastHeard { get; internal set; }
+		public bool IsFlashing { get; internal set; }
+		public string Version { get; internal set; }
+		public bool RequiresUpdate { get; internal set; }
+		public ParticleDeviceType Type { get; internal set; }
+
+		#endregion
+
+		#region public Methods
+		/// <summary>
+		/// Gets the value of the requested variable registered on Particle Device. 
+		/// Specific Variable will not be updated in 'Variable'. 
+		/// Use RefreshAsync() to update all the 'Variable' Values.
+		/// </summary>
+		/// <returns>The value of the requested variable registered on Particle Device</returns>
+		/// <param name="variableName">Variable name trying to get value of</param>
+		public async Task<ParticleVariableResponse> GetVariableAsync(string variableName)
+		{
+			try
+			{
+				using (var client = new HttpClient(new NativeMessageHandler()))
+				{
+					var response = await client.GetAsync(
+						"https://api.spark.io/v1/devices/" + Id + "/" + variableName + "?access_token=" + ParticleCloud.AccessToken.Token);
+					var particleArgs = DeserializeObject<ParticleVariableResponse>(await response.Content.ReadAsStringAsync());
+
+					LastApp = particleArgs?.Core?.LastApp;
+					return particleArgs ?? null;
+				}
+
+			}
+			catch (Exception e)
+			{
+				WriteLine(e.Message);
+			}
+
+			return null;
+		}
+		/// <summary>
+		/// Calls the registered function from the Particle Device.
+		/// </summary>
+		/// <returns>The function async.</returns>
+		/// <param name="functionName">Name of the function to invoke on Particle Device</param>
+		/// <param name="args">Parameter to be passed into the Particle Device function</param>
+		public async Task<string> CallFunctionAsync(string functionName, string args = null)
+		{
+			if (args?.Length > MAX_SPARK_FUNCTION_ARG_LENGTH)
+				throw new Exception("Argument length exceeded maximum allowable");
+
+			var requestContent = new FormUrlEncodedContent(new[] { new KeyValuePair<string, string>("arg", args) });
+
+			try
+			{
+				using (var client = new HttpClient(new NativeMessageHandler()))
+				{
+					var response = await client.PostAsync(
+						"https://api.spark.io/v1/devices/" + Id + "/" + functionName + "?access_token=" + ParticleCloud.AccessToken.Token,
+						requestContent);
+					var responseText = await response.Content.ReadAsStringAsync();
+					var particleResponse = DeserializeObject<ParticleFunctionResponse>(responseText);
+
+					if (IsNullOrEmpty(particleResponse.Id))
+					{
+						var error = DeserializeObject<ParticleErrorResponse>(responseText);
+						return error.Error;
+					}
+
+					return particleResponse?.Value.ToString() ?? "-1";
+				}
+
+			}
+			catch (Exception e)
+			{
+				WriteLine(e.Message);
+			}
+			return "-1";
+		}
+		/// <summary>
+		/// Refreshs the device. This includes the varibles and functions on the device. 
+		/// A return value of false indicates that ParticleCloud could not retreive the given device.
+		/// </summary>
+		/// <returns>A boolean indicating whether the refresh was successful or not.</returns>
+		public async Task<bool> RefreshAsync()
+		{
+			var updatedDevice = await ParticleCloud.SharedInstance.GetDeviceAsync(Id);
+
+			if (updatedDevice == null)
+				return false;
+
+			Id = updatedDevice?.Id;
+			Name = updatedDevice?.Name;
+			RequiresUpdate = updatedDevice.RequiresUpdate;
+			Variables = updatedDevice?.Variables;
+			Functions = updatedDevice?.Functions;
+			Version = updatedDevice?.Version;
+			LastHeard = updatedDevice.LastHeard;
+
+			return true;
+		}
+		/// <summary>
+		/// Sets the name of the device.
+		/// </summary>
+		/// <returns>A boolean indicating whether the name change was successful or not.</returns>
+		/// <param name="name">Name.</param>
+		public async Task<bool> SetNameAsync(string name)
+		{
+			var requestContent = new FormUrlEncodedContent(new[] { new KeyValuePair<string, string>("name", name) });
+
+			try
+			{
+				using (var client = new HttpClient(new NativeMessageHandler()))
+				{
+					var response = await client.PutAsync(
+						"https://api.spark.io/v1/devices/" + Id + "?access_token=" + ParticleCloud.AccessToken.Token,
+						requestContent);
+					var particleArgs = DeserializeObject<Dictionary<string, string>>(await response.Content.ReadAsStringAsync());
+
+					if (name == particleArgs["name"])
+					{
+						await RefreshAsync();
+						return true;
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				WriteLine(e.Message);
+			}
+
+			return false;
+		}
+		/// <summary>
+		/// Flashs the known app based on it's given name.
+		/// </summary>
+		/// <returns>A boolean indicating whether the flash was successful or not.</returns>
+		/// <param name="knownAppName">Known app name.</param>
+		public async Task<bool> FlashKnownAppAsync(string knownAppName)
+		{
+			var requestContent = new FormUrlEncodedContent(new[] {
+				new KeyValuePair<string, string> ("app", knownAppName),
+			});
+
+			try
+			{
+				using (var client = new HttpClient(new NativeMessageHandler()))
+				{
+					var response = await client.PutAsync(
+						"https://api.spark.io/v1/devices/" + Id + "?access_token=" + ParticleCloud.AccessToken.Token,
+						requestContent);
+					var particleArgs = DeserializeObject<ParticleFlashResponse>(await response.Content.ReadAsStringAsync());
+
+					if (particleArgs.Status == "Update started")
+						return true;
+				}
+
+			}
+			catch (Exception e)
+			{
+				WriteLine(e.Message);
+			}
+
+			return false;
+		}
+		/// <summary>
+		/// Unclaims the Particle Device.
+		/// </summary>
+		/// <returns>A boolean indicating whether the Particle Device was unclaimed successful or not.</returns>
+		public async Task<bool> UnclaimAsync()
+		{
+			try
+			{
+				using (var client = new HttpClient(new NativeMessageHandler()))
+				{
+					var response = await client.DeleteAsync(
+						"https://api.spark.io/v1/devices/" + Id + "?access_token=" + ParticleCloud.AccessToken.Token
+					);
+					var particleArgs = DeserializeObject<ParticleGeneralResponse>(await response.Content.ReadAsStringAsync());
+
+					if (particleArgs.Ok)
+						return true;
+				}
+			}
+			catch (Exception e)
+			{
+				WriteLine(e.Message);
+			}
+
+			return false;
+		}
+		/// <summary>
+		/// Flash a binary file to the ParticleDevice. 
+		/// </summary>
+		/// <returns>A boolean indicating whether the Particle Device was successfully flashed or not.</returns>
+		/// <param name="fileBytes">File in the form of a byte array</param>
+		/// <param name="filename">name of the binary file being flashed. This should include the '.bin'</param>
+		public async Task<bool> FlashFilesAsync(byte[] fileBytes, string filename)
+		{
+			var byteContent = new ByteArrayContent(fileBytes);
+			byteContent.Headers.ContentType = MediaTypeHeaderValue.Parse("application/octet-stream");
+
+			MultipartFormDataContent content = new MultipartFormDataContent();
+			content.Add(byteContent, "\"file\"", filename);
+
+			try
+			{
+				using (var client = new HttpClient(new NativeMessageHandler()))
+				{
+					var response = await client.PutAsync(
+						"https://api.spark.io/v1/devices/" + Id + "?access_token=" + ParticleCloud.AccessToken.Token,
+						content);
+					response.EnsureSuccessStatusCode();
+
+					var particleArgs = DeserializeObject<ParticleFlashResponse>(await response.Content.ReadAsStringAsync());
+
+					if (particleArgs.Status == "Update started")
+						return true;
+				}
+
+			}
+			catch (Exception e)
+			{
+				WriteLine(e.Message);
+			}
+
+			return false;
+		}
+
+		#endregion
+	}
+}
